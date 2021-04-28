42afe2596b80a0017c04e75052426e5f49a5507d
Karishma Savant <karishma.savant@gmail.com>
2021-04-28 13:03:03 +0530

    Doxygen comments in header of activity1.h

--- inc/activity1.h
+++ inc/activity1.h
@@ -1,3 +1,13 @@
+/**
+ * @file activity1.h
+ * @author KarishmaSavant
+ * @brief Header file for activity1, function declarations
+ * @version 0.1
+ * @date 2021-04-28
+ * 
+ * @copyright Copyright (c) 2021
+ * 
+ */
 #ifndef ACTIVITY1_H_INCLUDED
 #define ACTIVITY1_H_INCLUDED
 
